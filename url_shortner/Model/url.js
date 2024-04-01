6be63912d08b72bfcdaecb7236d6ade58e3f9611
Anshika <anshika1366@gmail.com>
2024-04-01 23:47:54 +0530

    Project url shortner

---
+++ url_shortner/Model/url.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    shortId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    redirectUrl: {
+        type: String,
+        required: true
+    },
+    visitHistory: [ { timestamp: {type: Number} } ]
+})
